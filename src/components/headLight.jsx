9de0c3cde5b75455b8d4bddc4b5258747300f95e
ashT95 <ash@moeyinc.com>
2022-04-10 11:27:26 -0400

    head light alternate fix

--- src/components/headLight.jsx
+++ src/components/headLight.jsx
@@ -2,6 +2,9 @@
 
 import { useEffect } from "react";
 
+var openPromiseList = [];
+var ch = [];
+
 function HeadLight() {
   useEffect(() => {
     const phidgetConnection = new phidget22.Connection(8989, "localhost");
@@ -12,17 +15,23 @@ function HeadLight() {
       .then(() => {
         console.log("Connected to Phidgets");
 
-        const led = new phidget22.DigitalOutput();
-        led.setDeviceSerialNumber(637638);
-        led.setHubPort(0);
-        //led.setIsHubPortDevice(true);
-        led.setChannel(15);
+        for (let i = 0; i < 16; i++) {
+          ch.push(new phidget22.DigitalOutput());
+        }
 
-        led
-          .open(3000)
+        ch[15].setDeviceSerialNumber(637638);
+        ch[15].setHubPort(0);
+        //ch[0].setIsHubPortDevice(true);
+        ch[15].setChannel(15);
+
+        ch[15].onAttach = onAttachHandler;
+        ch[15].onDetach = onDetachHandler;
+
+        openPromiseList.push(ch[15].open(5000));
+
+        Promise.all(openPromiseList)
           .then(() => {
-            phidgetDevices.push(led);
-            led.setState(true);
+            ch[15].setState(true);
           })
           .catch((err) => {
             console.error("Error opening LED: " + err);
@@ -33,10 +42,20 @@ function HeadLight() {
       });
 
     return () => {
+      ch[15].close();
       phidgetDevices.forEach((pd) => pd.close());
       phidgetConnection.close();
     };
   }, []);
+
+  function onAttachHandler(ch) {
+    var deviceSerialNumber = ch.getDeviceSerialNumber();
+  }
+
+  function onDetachHandler(ch) {
+    var deviceSerialNumber = ch.getDeviceSerialNumber();
+  }
+
 
   return <div></div>;
 }
